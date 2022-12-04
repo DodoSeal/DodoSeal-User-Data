05d6a74392e6aa307c79247f64cc3e1f506b2248
Max McDaniel <53157397+DodoSeal@users.noreply.github.com>
2022-12-04 09:06:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+app.get('/discord/:id', (req, res) => {
+    var userId = req.params.id;
+    if (!userId) {res.status(403)};
+    
+    fetch(`https://discord.com/api/v10/users/${userId}`, {
+    headers: {
+        Authorization: process.env.BOT_AUTH
+    }
+    }).then(data => data.json().then(response =>{
+        var avatar = response.avatar;
+        if (avatar) {
+            if (avatar.includes(`a_`)) {
+                res.send(`https://cdn.discordapp.com/avatars/${userId}/${response.avatar}.gif`);
+            } else {
+                res.send(`https://cdn.discordapp.com/avatars/${userId}/${response.avatar}`);
+            };
+        };
+    }));
+});
+
+app.get('/roblox/:username', (req, res) => {
+    var username = req.params.username;
+    if (!username) {res.status(403)};
+    
+    fetch(`https://api.roblox.com/users/get-by-username?username=${username}`).then(data => data.json().then(response =>{
+        res.json(response);
+    }));
+});
+
+app.listen(3000);

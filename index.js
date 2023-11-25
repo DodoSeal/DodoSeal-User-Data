fa8e93ca65a6ca671536f2baec8e2d23a6b6a7cb
Max McDaniel <maxhmcd08@gmail.com>
2023-11-25 09:04:45 -0700

    Add Cors for My Domain

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require(`cors`);
 const app = express();
+
+var corsOptions = {
+    origin: 'https://dodoseal.com',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 
 app.get('/discord/avatar/:id', (req, res) => {
     var userId = req.params.id;
